f5ca6c03f6c0af452602511dbf488778f651a81a
jahzzeel <jahzzeel@gmail.com>
2021-10-05 16:35:09 -0600

    test push from visual studio

--- test_calc.py
+++ test_calc.py
@@ -1,19 +1,19 @@
 import calc
 import unittest
 
+
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-
-        self.assertEqual(calc.add(10,5),15)
+        self.assertEqual(calc.add(10, 5), 15)
 
     def test_divide(self):
+        self.assertEqual(calc.divide(10, 2), 5, "The division is correct")
 
-        self.assertEqual(calc.divide(10,2),5)
-
-        #print("Ddddddd")
+        # print("Ddddddd")
         with self.assertRaises(ValueError):
-            calc.divide(10,0)
+            calc.divide(10, 0)
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     unittest.main()
--- test_employee.py
+++ test_employee.py
@@ -5,23 +5,26 @@ from employee import Employee
 class TestEmployee(unittest.TestCase):
 
 
+    
+
+
     @classmethod
     def setUpClass(cls):
         print("SETUP")
-        cls.emp_1=Employee('corey','Schafer',5000)
+        cls.emp_1 = Employee('corey', 'Schafer', 5000)
 
-    #def setUp(self):
+    # def setUp(self):
     #    print("SETUP")
     #    self.emp_1=Employee('corey','Schafer',5000)
 
     def test_email(self):
         print("//////")
-        self.assertEqual(self.emp_1.email,'corey.Schafer@email.com')
+        self.assertEqual(self.emp_1.email, 'corey.Schafer@email.com')
 
     def test_apply_rise(self):
         print("======")
         self.emp_1.apply_raise()
-        self.assertEqual(self.emp_1.pay,5250.00)
+        self.assertEqual(self.emp_1.pay, 5250.00)
 
 
 if __name__ == '__main__':
