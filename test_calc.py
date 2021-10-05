001aff6979afa86c4837eff0e461a1524b4a3cb1
jahzzeel <jahzzeel@gmail.com>
2021-10-05 11:42:40 -0600

    Test with assertRaises

--- test_calc.py
+++ test_calc.py
@@ -7,8 +7,13 @@ class TestCalc(unittest.TestCase):
 
         self.assertEqual(calc.add(10,5),15)
 
+    def test_divide(self):
 
+        self.assertEqual(calc.divide(10,2),5)
 
+        #print("Ddddddd")
+        with self.assertRaises(ValueError):
+            calc.divide(10,0)
 
 if __name__== '__main__':
     unittest.main()
