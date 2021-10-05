5ea715267d16bc21fe3803bbc4c0ec226a923b59
jahzzeel <jahzzeel@gmail.com>
2021-10-05 10:24:45 -0600

    First commit

---
+++ test_employee.py
@@ -0,0 +1,28 @@
+import unittest
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        print("SETUP")
+        cls.emp_1=Employee('corey','Schafer',5000)
+
+    #def setUp(self):
+    #    print("SETUP")
+    #    self.emp_1=Employee('corey','Schafer',5000)
+
+    def test_email(self):
+        print("//////")
+        self.assertEqual(self.emp_1.email,'corey.Schafer@email.com')
+
+    def test_apply_rise(self):
+        print("======")
+        self.emp_1.apply_raise()
+        self.assertEqual(self.emp_1.pay,5250.00)
+
+
+if __name__ == '__main__':
+    unittest.main()
